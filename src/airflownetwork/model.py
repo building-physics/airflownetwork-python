102ff177d823dbac389567b7a5b2672ba7312a6a
Jason W. DeGraw <jason.degraw@gmail.com>
2024-02-19 21:35:18 -0500

    Start toward solving

--- src/airflownetwork/model.py
+++ src/airflownetwork/model.py
@@ -1,4 +1,7 @@
 # SPDX-FileCopyrightText: 2023-present Oak Ridge National Laboratory, managed by UT-Battelle
 #
 # SPDX-License-Identifier: BSD-3-Clause
-__version__ = '1.1.0'
+import airnet
+
+class Model(airnet.Model):
+    pass
